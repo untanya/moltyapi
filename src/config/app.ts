df92ed59e26114494c9f106d24a370ce6d91844a
QuentinW59 <quentin.wisniewski@outlook.fr>
2025-05-05 15:33:54 +0200

    connexion à la base de données ok

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,1 +1,6 @@
 // ici je mets tout en rapport avec les routes des APIS
+import {authenticate} from "./db"
+
+export function init(){
+    authenticate();
+}
---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+// Option 2: Passing parameters separately (sqlite)
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: '.../database/moltyDB.sqlite'
+});
+
+export async function authenticate(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
